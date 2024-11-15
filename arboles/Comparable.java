cfec01a84ec01d7c242186e6e72186e14182a384
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-11-15 16:01:15 +0000

    interfase comparable

---
+++ arboles/Comparable.java
@@ -0,0 +1,6 @@
+package arboles;
+public interface Comparable {
+    public boolean esMayor(Object q);
+    public boolean esMenor(Object q);
+    public boolean esIgual(Object q);
+}
---
+++ arboles/TestComparable.java
@@ -0,0 +1,28 @@
+package arboles;
+
+public class TestComparable {
+    public static void main(String[] args) {
+        Estudiante estudiante1 = new Estudiante(1245,"Juan Abitia");
+        Estudiante estudiante2 = new Estudiante(1223,"Luis Castañeda");
+        if (estudiante1.equals(estudiante2))
+           System.out.println("Los estudiantes son iguales");
+        else if (estudiante1.esMayor(estudiante2))
+           System.out.println(estudiante1.getCodigo()+" es mayor que "+estudiante2.getCodigo());
+        else
+            System.out.println(estudiante1.getCodigo()+" es menos que "+estudiante2.getCodigo());
+
+        Entero entero1 = new Entero(5);
+        Entero entero2 = new Entero(10);
+
+        if (entero1.equals(entero2))
+           System.out.println("Los números son iguales");
+        else if (entero1.esMayor(entero2))
+           System.out.println(entero1.getDato()+"  es mayor que "+entero2.getDato());
+        else
+           System.out.println(entero1.getDato()+"  es menor que "+entero2.getDato());
+
+
+
+    }
+    
+}
