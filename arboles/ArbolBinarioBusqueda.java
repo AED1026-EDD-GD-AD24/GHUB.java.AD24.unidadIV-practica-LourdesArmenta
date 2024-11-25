8635d3249e81fc50df8e8435c3b89d91a9caea64
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-11-25 16:00:02 +0000

    pendiente de desarrollar reemplazarPorMayorIzquierdo

--- arboles/ArbolBinarioBusqueda.java
+++ arboles/ArbolBinarioBusqueda.java
@@ -74,5 +74,113 @@ public class ArbolBinarioBusqueda extends ArbolBinario{
         return raizSub;
 
     }
+
+    /**
+     * 
+     * @param valor, valor a buscar
+     * @return el nodo encontrado
+     */
+    public Nodo buscar(Object valor){
+        Comparable dato = (Comparable)valor;
+        if (raiz == null)
+           return raiz;
+        else{
+            //aux: auxiliar que va recorriendo los nodos, de la la raiz
+            Nodo aux = raiz;
+            while (aux !=null){
+                if (dato.esIgual(aux.getValor()))
+                   return aux;
+                if (dato.esMenor(aux.getValor()))
+                   aux = aux.getIzquierdo();
+                else 
+                   aux = aux.getDerecho();
+                   
+            }
+            return null;
+        }
+    }
+
+    /**
+     * Interfaz de buscar que invoca al metodo recuersivo localizar
+     * @param buscado, valor buscado
+     * @return Nodo buscado o null si no lo encuentra
+     */
+    public Nodo buscar2(Object buscado){
+        Comparable dato = (Comparable)buscado;
+        if (raiz == null)
+          return null;
+        else
+           return localizar(raiz,dato);
+    }
+
+    /**
+     * Version recursiva
+     * @param raizSub
+     * @param buscado
+     * @return Nodo buscado si lo encuentra, si no retorna null
+     */
+
+    private Nodo localizar(Nodo raizSub, Comparable buscado){
+        if (raizSub == null)
+           return null;
+        else if (buscado.esIgual(raizSub.getValor()))
+            return raizSub;
+        else if (buscado.esMenor(raizSub.getValor()))
+            return localizar(raizSub.getIzquierdo(), buscado);
+        else 
+            return localizar(raizSub.getDerecho(), buscado);
+
+    }
+    public boolean eliminar(Object valor){
+        Comparable dato = (Comparable)valor;
+        //Buscar el nodo a eliminar y su antecesor
+        Nodo antecesor = null; //antecesor del nodo a eliminar
+        //aux: auxiliar que va recorriendo los nodos, desde la raiz
+        Nodo aux = raiz;
+        while (aux !=null){
+            if (dato.esIgual(aux.getValor())){
+                break;
+            }
+            antecesor = aux;
+            if (dato.esMenor(aux.getValor()))
+                aux = aux.getIzquierdo();
+            else 
+                aux = aux.getDerecho();
+            
+
+        }
+        if (aux == null){
+            return false;
+        }
+        //si llega a este punto, el nodo a eliminar existe y es aux y su antecesor es antecesor
+        //Exainar cada caso
+        //1. si tiene menos de dos hijos, incluso una hoja
+        if (aux.getIzquierdo() == null) //solo tiene hijo derecho
+            if(((Comparable) aux.getValor()).esMenor(antecesor.getValor()))
+                antecesor.setIzquierdo(aux.getDerecho());
+               
+            else
+                antecesor.setDerecho(aux.getDerecho());
+        else if (aux.getDerecho()==null)//solo tiene hijo izquierdo
+            if(((Comparable) aux.getValor()).esMenor(antecesor.getValor()))
+                 antecesor.setIzquierdo(aux.getIzquierdo());
+            else
+                 antecesor.setDerecho(aux.getIzquierdo());
+        else 
+            //El nodo a eliminar tiene rama izquierda y rama derecha
+            reemplazarPorMayorIzquierdo(aux);
+        aux =null;
+        return true;
+
+
+    }
+    /**
+     * Reemplaza el nodo actual por el mayor del la rama izquierda
+     * 
+     * @param act nodo Actual o nodo a eliminar que tiene rama izquierda y rama derecha
+     */
+    public void reemplazarPorMayorIzquierdo(Nodo act){
+        //AQUI NOS QUEDAMOS
+    }
     
 }
