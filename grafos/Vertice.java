1874e921d81c0893520ce64ca6cf78b31f6eafc9
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-12-02 15:49:10 +0000

    grafos

---
+++ grafos/Vertice.java
@@ -0,0 +1,18 @@
+package grafos;
+//Representar un nodo del grafo, con su nombre
+public class Vertice{
+    private String nombre;
+    public Vertice(String nombre){
+        this.nombre = nombre;
+
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+
+
+}
